632801586f58eca8d7b98694a711ffaa2213b050
José Carlos Palma <palmahn@gmail.com>
2021-09-29 23:50:02 -0600

    refactor(app): Limpieza de archivos iniciales.

---
+++ src/todo/ToDo.tsx
@@ -0,0 +1,14 @@
+export const ToDo = () => {
+    return (
+        <>
+            <h1>ToDo App</h1>
+            <hr />
+            <ul>
+                <li>Tarea 1</li>
+                <li>Tarea 2</li>
+                <li>Tarea 3</li>
+                <li>Tarea 4</li>
+            </ul>
+        </>
+    );
+};
